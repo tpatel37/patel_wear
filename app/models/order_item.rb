c0e5b47fd2ea1fcc1a38c699358eb17a7d5335ca
tpatel37 <tpatel37@rrc.ca>
2025-04-23 11:42:42 -0500

    Added helper method for category name

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,4 +4,4 @@ class OrderItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :price, presence: true, numericality: true
 
-en
+end
